a8d5e742a2563824f554d3daff4f100cf6c29591
LennertDefauw <lennert.defauw@jimber.org>
2022-06-16 12:12:55 +0200

    android

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,108 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:yggdrasil_plugin/yggdrasil_plugin.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: TestPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class TestPage extends StatelessWidget {
+  String _platformVersion = 'Unknown';
+  YggdrasilPlugin plugin = YggdrasilPlugin();
+  BuildContext? _context;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  TestPage() {
+    plugin.setOnReportIp(reportIp);
+    initPlatformState();
+  }
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context) {
+    _context = context;
+    return Scaffold(
+      appBar: AppBar(title: Text("Test")),
+      body: Container(
+          child: new Row (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  color: Colors.redAccent,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    startVPN(context);
+                  },
+                  child: Text("Start"),
+                ),
+                RaisedButton(
+                  color: Colors.redAccent,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    stopVPN(context);
+                  },
+                  child: Text("Stop"),
+                )
+              ])
+      ),
+    );
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  showAlertDialog(String message) {
+    showDialog(
+      context: _context!,
+      builder: (_) => new AlertDialog(
+        title: const Text("Your IP"),
+        content: new Text(message),
+        actions: [
+          new FlatButton(
+            child: const Text("Ok"),
+            onPressed: () {
+              Navigator.pop(_context!);
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void reportIp(String ip) {
+    print("EVEN IN THE UI FFS...");
+    showAlertDialog(ip);
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    platformVersion = await plugin.platformVersion();
+  }
+
+  void startVPN(BuildContext context) async {
+    await plugin.startVpn({
+      'signingPublicKey': '620ed6d70c54fdf4accfdadef94a60091f791664566d7423fa4877a62bdc60cc',
+      'signingPrivateKey': '760b5fbb02cfec5fd1899d943060957a17ba5a6383402b7e6c363bb6bcddd451620ed6d70c54fdf4accfdadef94a60091f791664566d7423fa4877a62bdc60cc',
+      'encryptionPublicKey': '36fc3adb1a3f0a1d62d8e721e37e66e59cbcc0ca347043e82c5ed248f72a6a04',
+      'encryptionPrivateKey': 'f8cff6bb9ffdcf196dca5d7c9aebe97c2bcf6ca554e7110a9f9f597b9b2a7975'
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void stopVPN(BuildContext context) async {
+    await plugin.stopVpn();
   }
 }
